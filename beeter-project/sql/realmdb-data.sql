8e22658733ba7bcd0d8f9f7b7fd2311282b35852
raullorenzo <raul.lorenzo.67@gmail.com>
2014-11-14 03:28:49 +0100

    Update realmdb-data.sql

--- beeter-project/sql/realmdb-data.sql
+++ beeter-project/sql/realmdb-data.sql
@@ -4,4 +4,4 @@ insert into users values('alicia', MD5('alicia'), 'Alicia', 'alicia@acme.com');
 insert into user_roles values ('alicia', 'registered');
 
 insert into users values('blas', MD5('blas'), 'Blas', 'blas@acme.com');
-insert into user_roles values ('blas', 'registered');
+insert into user_roles values ('blas', 'registered'); 
